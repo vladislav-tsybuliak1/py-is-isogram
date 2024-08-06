5149a923bea14044621e386ee0b45c38a0064fd4
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-08-06 15:18:11 +0300

    write test for is_isogram function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 def is_isogram(word: str) -> bool:
+    validate(word)
+
     word_lower = word.lower()
     for letter in word_lower:
         if word_lower.count(letter) > 1:
             return False
     return True
+
+
+def validate(word: str) -> None:
+    if not isinstance(word, str):
+        raise TypeError
+
+    for char in word:
+        if not char.isalpha():
+            raise ValueError
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,50 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,check_isogram",
+    [
+        ("", True),
+        ("a", True),
+        ("A", True),
+        ("playgrounds", True),
+        ("PlAyGrOuNdS", True),
+        ("look", False),
+        ("LOOK", False),
+        ("Adam", False),
+        ("adam", False),
+        ("ADAM", False)
+    ]
+)
+def test_expected_result(word: str, check_isogram: bool) -> None:
+    assert is_isogram(word) == check_isogram
+
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        1, 1.5, False, ["a"], {"a": "a"}, {"a"}, ("a",)
+    ]
+)
+def test_invalid_input_type(word: str) -> None:
+    with pytest.raises(TypeError):
+        is_isogram(word)
+
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        "134",
+        "1Adam",
+        "sdf_!%",
+        "\nfr",
+        "Hello,.",
+        " ",
+        "adam "
+    ]
+)
+def test_invalid_input_value(word: str) -> None:
+    with pytest.raises(ValueError):
+        is_isogram(word)
